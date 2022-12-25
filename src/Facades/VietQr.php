3a6d589184cbd67a31883bb06ae3c905e7e5a99f
Son Banh <takashato@gmail.com>
2022-12-25 15:06:49 +0700

    bind for VietQr

--- src/Facades/VietQr.php
+++ src/Facades/VietQr.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Facade;
  */
 class VietQr extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return \Takashato\VietQr\VietQr::class;
     }
